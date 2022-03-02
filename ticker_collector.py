5d6c9abf7d218af1fb3b5817c4cdea29e9c89fd4
Photon1c <35161340+Photon1c@users.noreply.github.com>
2022-03-02 00:44:22 -0800

    Update and rename ticket_collector.py to ticker_collector.py
    updated name and added additional functionality to ticker_collector

---
+++ ticker_collector.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_html("https://www.marketwatch.com/tools/screener/after-hours")
+dfg = df[0]
+#create new series that keeps only first string value in 'Symbol Symbol' column, discarding anything after space
+dfgainers = dfg['Symbol  Symbol'].str[:4]
+dflosers = df[1]
+dflosers2 = dflosers['Symbol  Symbol'].str[:4]
+dfgainers2 = dfgainers
+df_final = dfgainers2.append(dflosers2)
+df_final2 = pd.DataFrame()
+df_final2["Symbol"] = df_final
+df_final2["Symbol"].to_csv(r'[path to your csv file with stock tickers]')
+
+
+#test to see if full dataframe can be extracted by printing main frame
+#print(dfg, dgl) 
+#test final list
+print(df_final2)
